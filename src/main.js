b4a5e4b81f0ddf1b444e87b03e0f674f053cca42
Zim <daniil.kudlach2018@gmail.com>
2020-02-06 20:18:58 +0200

    Add object controller

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import {StarsController} from "./stars/StarsController.js";
-import {Publisher} from "./share/Publisher.js";
+import { StarsController } from "./stars/StarsController.js";
+import { Publisher } from "./share/Publisher.js";
 import { MoveAnimationsController } from "./animations/MoveAnimationsController.js";
 import { UserObjectController } from "./objects/UserObjectController.js";
+import { ObjectController} from "./objects/ObjectController.js";
 
 const publisher = new Publisher();
 const methods = publisher.getMethods();
 const star = new StarsController(methods);
 const moveAnimations = new MoveAnimationsController(methods);
+const userObject = new ObjectController(methods);
 // const userObject = new UserObjectController(methods);
