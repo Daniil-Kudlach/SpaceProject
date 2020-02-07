979ced73c1adb4a6b34316ae3d27e337ad89fdbf
Zim <daniil.kudlach2018@gmail.com>
2020-02-07 11:39:55 +0200

    add controllers for Events, Moving, Stars, Objects

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { StarsController } from "./stars/StarsController.js";
 import { Publisher } from "./share/Publisher.js";
+import { EventsProcessingController } from "./animations/EventsProcessingController.js";
 import { MoveAnimationsController } from "./animations/MoveAnimationsController.js";
-import { UserObjectController } from "./objects/UserObjectController.js";
-import { ObjectController} from "./objects/ObjectController.js";
+import { UserObjectController } from "./objects/user/UserObjectController.js";
+import { StarsController } from "./objects/background/StarsController.js";
+import { ObjectsGeneratorController } from "./objects/ObjectsGeneratorController.js";
 
 const publisher = new Publisher();
 const methods = publisher.getMethods();
-const star = new StarsController(methods);
-const moveAnimations = new MoveAnimationsController(methods);
-const userObject = new ObjectController(methods);
-// const userObject = new UserObjectController(methods);
+const eventsProcessing = new EventsProcessingController(methods);
+const moveAnimation = new MoveAnimationsController(methods);
+const stars = new StarsController(methods);
+const generator = new ObjectsGeneratorController(methods);
+const userObject = new UserObjectController(methods);
