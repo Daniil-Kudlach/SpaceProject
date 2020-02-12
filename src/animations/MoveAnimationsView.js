1032ba954581b8f03ff18e995c662d970ea9457a
Zim <daniil.kudlach2018@gmail.com>
2020-02-12 15:05:25 +0200

    Rmv method DrawStar, and timeout-debouncer

--- src/animations/MoveAnimationsView.js
+++ src/animations/MoveAnimationsView.js
@@ -2,8 +2,8 @@ export class MoveAnimationsView {
     constructor() {
         this.canvas = document.querySelector("#canvas");
         this.ctx = canvas.getContext("2d");
-        this.inter;
-        this.timeout;
+        this.ctx.imageSmoothingEnabled = true;
+        this.inter  = true;
         this.requestAnimationFrame = window.requestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
@@ -22,14 +22,9 @@ export class MoveAnimationsView {
 
     go(fnc) {
         if (this.inter) {
-            clearTimeout(this.timeout);
-            this.timeout = false;
             cancelAnimationFrame(this.inter);
-            this.inter = false;
+            this.inter = requestAnimationFrame(fnc, this.canvas)
         }
-        this.timeout = setTimeout(() => {
-            this.inter = requestAnimationFrame(fnc, this.canvas);
-        }, 50)
     }
 
     stop() {
@@ -37,23 +32,10 @@ export class MoveAnimationsView {
         this.inter = false;
     }
 
-    drawStar(star) {
-        star.draw = (layer) => {
-            this.ctx.beginPath();
-            this.ctx.arc(star.x, star.y, star.radius, star.start, star.end, true);
-            this.ctx.fillStyle = star.color;
-            this.ctx.shadowColor = star.color;
-            this.ctx.shadowBlur = 15;
-            this.ctx.fill();
-            this.ctx.closePath();
-        }
-        return star;
-    }
-
     getParam() {
         return {
-            w: this.canvas.width = window.innerWidth,
-            h: this.canvas.height = window.innerHeight,
+            w: this.canvas.width = innerWidth,
+            h: this.canvas.height = innerHeight,
             c: this.canvas,
             ctx: this.ctx
         }
