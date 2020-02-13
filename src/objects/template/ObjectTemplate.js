81a339f3b969b1e25a6b1732c2bf88aa6261d3d7
Zim <daniil.kudlach2018@gmail.com>
2020-02-13 16:10:49 +0200

    Add methods for evolution, mass, child, orbit

--- src/objects/template/ObjectTemplate.js
+++ src/objects/template/ObjectTemplate.js
@@ -1,16 +1,16 @@
 export class ObjectTemplate {
     constructor(ctx, param) {
         this.type = param.src[param.mass - 1];
+        this.src = param.src;
         this.mass = param.mass;
+        this.percent = 0;
+        this.collision = false;
         this.child = [];
+        this.parent;
         this.m = param.m;
         this.ctx = ctx;
         this.img = new Image();
-        this.img.src = `../../../img/${param.type}.png`;
-        this.img.onload = () => {
-            this.draw();
-            this.halfWidth = this.img.width / 2;
-        }
+        this.img.src = `../../../img/${this.type}.png`;
         this.dir = {
             x: param.dir.x,
             y: param.dir.y
@@ -21,116 +21,43 @@ export class ObjectTemplate {
         this.speed = 2.9;
         this.blur = false;
         this.PI = Math.PI * 2;
-        this.rotationSpeed = Math.random() > 0.5 ? -Math.sqrt(Math.random() * 10) : Math.sqrt(Math.random() * 10) * Math.PI / 360;
-        this.x = param.x - this.img.width / 2;
-        this.y = param.y - this.img.height / 2;
+        this.rotationSpeed = Math.random() > 0.5 ? -Math.sqrt(Math.random() * 2) * Math.PI / 360 : Math.sqrt(Math.random() * 2) * Math.PI / 360;
+        this.x = param.x;
+        this.y = param.y;
+        this.center = {};
+        this.img.onload = () => {
+            this.loadImg();
+        }
+    }
+
+    loadImg() {
+        this.halfWidth = this.img.width / 2;
         this.center = {
-            x: this.halfWidth + this.x,
-            y: this.halfWidth + this.y
+            x: this.x,
+            y: this.y
         };
+        this.draw();
     }
 
     draw(par) {
-        if (par && this.moving) {
-            this.changePosition(par);
-        }
-        if (!this.blur) {
+        if (par && !this.collision && !this.orb) {
+            this.moving ? this.changePosition(par) : 0;
             this.ctx.save();
-            this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.width);
+            this.ctx.drawImage(this.img, this.x - this.halfWidth, this.y - this.halfWidth, this.img.width, this.img.width);
             this.ctx.restore();
-        } else {
-            this.ctx.save();
-            this.ctx.shadowBlur = 20;
-            this.ctx.shadowColor = 'white';
-            this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.width);
-            this.ctx.restore();
-            this.blur = false;
-        }
-    }
-
-    court(obj) {
-        console.log(this);
-        if (this.mass <= 5) {
-            this.addMass(obj.mass);
-            obj.newPosition(Math.random() * 10000, Math.random() * 10000);
-        } else if (this.mass <= 7 && this.mass > 5) {
-            if (obj.mass <= 5) {
-                obj.orbit();
-                this.addChild(obj);
-            } else {
-                obj.dir.x = this.dir.x;
-                obj.dir.y = this.dir.y;
+            if (this.child.length !== 0) {
+                this.child.length >= 5 ? this.eat(this.child[0]) : 0;
             }
-        } else if (this.mass <= 11 && this.mass > 7) {
-            if (obj.mass <= 5) {
-                this.addChild(obj.orbit());
-            } else {
-                this.addMass(obj.mass);
-                obj.newPosition(Math.random() * 10000, Math.random() * 10000);
-            }
-        }
-    }
-
-    evolution(type) {
-        this.type = param.src[mass];
-        this.img.src = `../../../img/${this.type}.png`;
-    }
-
-    addMass(mass) {
-        this.mass == mass / 10;
-        switch (this.mass) {
-            case 2:
-                this.evolution(1);
-                break;
-            case 3:
-                this.evolution(2);
-                break;
-            case 4:
-                this.evolution(3);
-                break;
-            case 5:
-                this.evolution(4);
-                break;
-            case 6:
-                this.evolution(5);
-                break;
-            case 7:
-                this.evolution(6);
-                break;
-            case 8:
-                this.evolution(7);
-                break;
-            case 9:
-                this.evolution(8);
-                break;
-            case 10:
-                this.evolution(9);
-                break;
-            case 11:
-                this.evolution(10);
-                break;
-        }
-        this.blur = true;
-    }
-
-    addChild(child) {
-        this.child.push(child);
-    }
-
-    orbit() {
-        this.orb = true;
-    }
-
-    OrbitMove(par) {
-        if (this.orb) {
+        } else if (this.orb && this.moving) {
             this.ctx.save();
-            this.ctx.translate(par.x, par.y); //вращение вокруг центра
+            this.ctx.translate(this.parent.x, this.parent.y); //вращение вокруг центра
             this.ctx.rotate((this.degree += this.rotationSpeed));
-            this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
+            this.ctx.drawImage(this.img, this.orb - this.halfWidth, this.orb- this.halfWidth, this.img.width, this.img.height);
             this.ctx.beginPath();
-            this.ctx.moveTo(0, 0);
-            this.ctx.lineTo(this.x + this.halfWidth, this.y + this.halfWidth);
-            this.ctx.arc(this.x + this.halfWidth, this.y + this.halfWidth, this.halfWidth, 0, this.PI);
+            this.ctx.moveTo(this.orb, this.orb);
+            this.ctx.arc(this.orb, this.orb, this.halfWidth, 0, this.PI);
+            this.ctx.moveTo(this.orb, this.orb);
+            this.ctx.lineTo(0, 0)
             this.ctx.strokeStyle = 'rgba(255,255,255,0.1)';
             this.ctx.lineWidth = 3;
             this.ctx.stroke();
@@ -140,27 +67,174 @@ export class ObjectTemplate {
         }
     }
 
+    court(obj) {
+        if (this.mass <= 5) {
+            this.addMass(obj.mass);
+            obj.newPosition(Math.random() * 10000, Math.random() * 10000);
+            this.collision = false;
+            this.shadow('orange');
+        } else if (this.mass <= 7 && this.mass > 5) {
+            if (obj.mass <= 5) {
+                obj.orbit(this);
+                this.addChild(obj);
+                this.shadow('orange');
+            } else {
+                obj.dir.x = this.dir.x;
+                obj.dir.y = this.dir.y;
+            }
+            this.collision = false;
+        } else if (this.mass <= 11 && this.mass > 7) {
+            if (obj.mass <= 5) {
+                obj.orbit(this)
+                this.addChild(obj);
+                this.shadow('orange');
+            } else {
+                this.addMass(obj.mass);
+                obj.newPosition(Math.random() * 10000, Math.random() * 10000);
+                this.shadow('orange');
+            }
+            this.collision = false;
+        }
+    }
+
+    shadow(color) {
+        this.ctx.save();
+        this.ctx.shadowBlur = 20;
+        this.ctx.shadowColor = color;
+        this.ctx.drawImage(this.img, this.x - this.halfWidth, this.y - this.halfWidth, this.img.width, this.img.width);
+        this.ctx.restore();
+        this.blur = false;
+        return;
+    }
+
+    evolution(type) {
+        this.type = this.src[type];
+        this.img = new Image();
+        this.img.src = `../../../img/${this.type}.png`;
+        this.img.onload = () => {
+            this.loadImg();
+            if(this.child.length > 0){this.child.forEach((e)=>{this.eat(e)})};
+        }
+        this.blur = true;
+        return;
+    }
+
+    minusMass(mass) {
+        this.moving ? console.log(this.mass) : 0;
+        this.mass > 2 ? this.percent -= mass : 0;
+        if (this.percent < 0) {
+            this.mass -= 1;
+            this.checkMass();
+            this.percent = 0;
+        }
+        return;
+    }
+
+    addMass(mass) {
+        this.percent += mass / this.mass;
+        if (this.percent >= 100) {
+            this.mass += 1;
+            this.checkMass();
+            this.percent = 0;
+        }
+        return;
+    }
+
+    checkMass() {
+        if (this.mass > 0 && this.mass < 2) {
+            this.evolution(0);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 2 && this.mass < 3) {
+            this.evolution(1);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 3 && this.mass < 4) {
+            this.evolution(2);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 4 && this.mass < 5) {
+            this.evolution(3);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 5 && this.mass < 6) {
+            this.evolution(4);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 6 && this.mass < 7) {
+            this.evolution(5);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 7 && this.mass < 8) {
+            this.evolution(6);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 8 && this.mass < 9) {
+            this.evolution(7);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 9 && this.mass < 10) {
+            this.evolution(8);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 10 && this.mass < 11) {
+            this.evolution(9);
+            this.shadow('green');
+            return;
+        } else if (this.mass >= 11) {
+            this.evolution(10);
+            this.shadow('green');
+            return;
+        } else {
+            return;
+        }
+    }
+
+    addChild(child) {
+        this.child.push(child);
+        this.blur = true;
+        return;
+    }
+
+    orbit(parent) {
+        this.orb = this.img.width * 4;
+        this.parent = parent;
+        return;
+    }
+
     newPosition(x, y) {
-        this.x = x;
-        this.y = y;
-        this.draw();
+        if (!this.orb) {
+            this.x = x;
+            this.y = y;
+            this.draw();
+        }
+        return;
     }
 
     changePosition(par) {
         this.x -= par.dir.x * (this.speed / par.div) + this.dir.x;
         this.y -= par.dir.y * (this.speed / par.div) + this.dir.y;
-        if (this.child.length !== 0) {
-            this.child.forEach(el => el.orbitMove({
-                x: this.x,
-                y: this.y
-            }));
-        }
-        this.center.x = this.halfWidth + this.y;
+        this.center.x = this.halfWidth + this.x;
         this.center.y = this.halfWidth + this.y;
+        return;
+    }
+
+    rmwChild(c) {
+        this.child.splice(this.child.indexOf(c), 1);
+        return;
+    }
+
+    eat(child) {
+        child.orb = false;
+        this.addMass(child.mass);
+        child.newPosition(Math.random() * 10000, Math.random() * 10000);
+        this.rmwChild(child);
+        return;
     }
 
     changeMiddle(m) {
         this.m = m;
+        return;
     }
 
 }
