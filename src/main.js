f6a54fb906bdac6caf6381367f73706d39b9acec
Zim <daniil.kudlach2018@gmail.com>
2020-02-13 16:11:38 +0200

    Delete import of UserObjectController

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { Publisher } from "./share/Publisher.js";
 import { EventsProcessingController } from "./animations/EventsProcessingController.js";
 import { MoveAnimationsController } from "./animations/MoveAnimationsController.js";
-import { UserObjectController } from "./objects/user/UserObjectController.js";
 import { StarsController } from "./objects/background/StarsController.js";
 import { ObjectsGenerator } from "./objects/ObjectsGenerator.js";
 
@@ -11,4 +10,3 @@ const eventsProcessing = new EventsProcessingController(methods);
 const moveAnimation = new MoveAnimationsController(methods);
 const stars = new StarsController(methods);
 const generator = new ObjectsGenerator(methods);
-const userObject = new UserObjectController(methods);
